e0e84ea9baa1f69b82744ee769441f6087b11ee9
DavidFC0523 <90779177+DavidFC0523@users.noreply.github.com>
2022-01-27 10:42:41 +0100

    commit

---
+++ src/main/java/es/albarregas/beans/Direccion.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.albarregas.beans;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author gared
+ */
+@Entity
+@Table(name = "direcciones1a1AFK")
+
+public class Direccion implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idDirec")
+    private int id;
+    @Column(name = "calle", length = 40, nullable = false)
+
+    private String calle;
+    @Column(name = "numero", nullable = false)
+    private int numero;
+    @Column(name = "poblacion", length = 30, nullable = false)
+
+    private String poblacion;
+    @Column(name = "provincia", length = 30, nullable = false)
+
+    private String provincia;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getPoblacion() {
+        return poblacion;
+    }
+
+    public void setPoblacion(String poblacion) {
+        this.poblacion = poblacion;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public Direccion() {
+    }
+
+    public Direccion(int id, String calle, int numero, String poblacion, String provincia) {
+        this.id = id;
+        this.calle = calle;
+        this.numero = numero;
+        this.poblacion = poblacion;
+        this.provincia = provincia;
+    }
+
+}
